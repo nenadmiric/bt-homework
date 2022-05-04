2fa4a613d8d723c79bfca01b38b2095f65026959
nenadmiric <nenadmiric3@gmail.com>
2022-05-04 12:39:09 +0200

    second commit

---
+++ hw10.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+Zadatak 1
+$a = 200;
+$b = 80;
+$zbir = $a + $b;
+$razlika = $a - $b;
+$proizvod = $a * $b;
+$kolicnik = $a / $b;
+echo "Zbir dva broja je " . $zbir;
+echo "<br>";
+echo "Razlika dva broja je " . $razlika;
+echo "<br>";
+echo "Proizvod dva broja je " . $proizvod;
+echo "<br>";
+echo "Kolicnik dva broja je " . $kolicnik;
+*/
+
+/*
+Zadatak 2
+$dan=rand(0,6);
+if ($dan == "0") {
+    echo "Danas je ponedeljak";
+  } elseif ($dan == "1") {
+    echo "Danas je utorak";
+  } elseif ($dan == "2") {
+    echo "Danas je sreda";
+  } elseif ($dan == "3") {
+      echo "Danas je cetvrtak";
+  } else if ($dan == "4") {
+      echo "Danas je petak";
+  } else if ($dan == "5") {
+      echo "Danas je subota";
+  } else {
+      echo "Danas je nedelja";
+  }
+*/
+
+/*
+Zadatak 3
+$a = 5;
+$b = 7;
+$c = 9;
+$d = $a + $b + $c;
+echo "Zbir brojeva " . $a . ", " . $b . " i " . $c . " je " . $d . ".";
+*/
+
+?>
